e023565b465582f40b5d9e8cc0af5486c607f253
Orbitado <juanpaavaldezz@gmail.com>
2025-04-06 20:11:06 -0300

    feat: Add dotenv support and MongoDB connection configuration
    - Introduced a new `.env.example` file for environment variable configuration.
    - Updated `.gitignore` to exclude environment files.
    - Added `dotenv` package to manage environment variables.
    - Configured MongoDB connection using `MONGO_URI` from environment variables in `app.js`.
    - Improved error handling for missing `MONGO_URI`.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,39 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
-import usersRouter from './routes/users.router.js';
-import petsRouter from './routes/pets.router.js';
-import adoptionsRouter from './routes/adoption.router.js';
-import sessionsRouter from './routes/sessions.router.js';
-import mocksRouter from './routes/mocks.router.js';
+import usersRouter from "./routes/users.router.js";
+import petsRouter from "./routes/pets.router.js";
+import adoptionsRouter from "./routes/adoption.router.js";
+import sessionsRouter from "./routes/sessions.router.js";
+import mocksRouter from "./routes/mocks.router.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`mongodb://localhost:27017/adoptme`)
+const PORT = process.env.PORT || 8080;
+
+if (!process.env.MONGO_URI) {
+  throw new Error("MONGO_URI is not defined");
+}
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB successfully!");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
-app.use('/api/mocks',mocksRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/pets", petsRouter);
+app.use("/api/adoptions", adoptionsRouter);
+app.use("/api/sessions", sessionsRouter);
+app.use("/api/mocks", mocksRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
